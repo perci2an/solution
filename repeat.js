983aa3602efca228fa03bd7e1a7de502b9008ebe
perci2an <122770473+perci2an@users.noreply.github.com>
2023-08-07 10:50:54 +0900

    2023.08.07

--- repeat.js
+++ repeat.js
@@ -1,7 +1,9 @@
 function solution(my_string, n) {
-  let repeatedString = "";
+  // 매개변수 'my_string'과 'n'을 입력받음
+  let repeatedString = ""; // 빈 문자열 생성
   for (let char of my_string) {
-    repeatedString += char.repeat(n);
+    // my_string의 요소들을 순서대로 가져와 char에 할당
+    repeatedString += char.repeat(n); // 현재 할당된 알파벳을 n번만큼 반복하여 repeatedString에 할당
   }
   return repeatedString;
 }
